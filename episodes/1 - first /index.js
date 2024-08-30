4b1c2750685560bbcca95e9d68a7ae8d47ea3b92
Fazle Khan <fazleskhan@gmail.com>
2024-08-30 17:54:35 -0400

    moved first episode work into 1 - first directory

---
+++ episodes/1 - first /index.js
@@ -0,0 +1,12 @@
+console.log("Hellow World!");
+
+const {Client, Events, GatewayIntentBits} = require("discord.js");
+const {token} = require("./config.json");
+
+const client = new Client({intents: [GatewayIntentBits.Guilds]});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Logged ion as ${c.user.tag}`);
+})
+
+client.login(token);

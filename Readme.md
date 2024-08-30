e23adef4859669f48fad006328359ef304b824d2
Fazle Khan <fazleskhan@gmail.com>
2024-08-30 15:28:41 -0400

    second tutorial

--- Readme.md
+++ Readme.md
@@ -1,6 +1,14 @@
 # Work for Golden Development's Discord bot tutorial
 
+https://development.goldendev.net/
+
 https://www.youtube.com/playlist?list=PLbeTcejqhlQRJDdICA8pWmbuqivgc9aEq
 
 ## First Tutorial
+## Code Your Own Discord Bot | Basics | discord.js v14 (2023)
 https://www.youtube.com/watch?v=eHssMfhVXwk&list=PLbeTcejqhlQRJDdICA8pWmbuqivgc9aEq&index=1
+
+## Second Tutorial
+### Code Your Own Discord Bot | Basic Slash Command Handler | discord.js v14 (2023)
+
+https://www.youtube.com/watch?v=dApRecz4BDc&list=PLbeTcejqhlQRJDdICA8pWmbuqivgc9aEq&index=2
--- index.js
+++ index.js
@@ -1,12 +1,35 @@
-console.log("Hellow World!");
+console.log("Hello World!");
 
-const {Client, Events, GatewayIntentBits} = require("discord.js");
-const {token} = require("./config.json");
+const { Client, Events, GatewayIntentBits, SlashCommandBuilder } = require('discord.js');
+const { token } = require("./config.json");
 
-const client = new Client({intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.once(Events.ClientReady, c => {
-  console.log(`Logged ion as ${c.user.tag}`);
-})
+  console.log(`Logged in as ${c.user.tag}`);
+
+  const ping = new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with "Pong!"');
+
+  const hello = new SlashCommandBuilder()
+        .setName('hello')
+        .setDescription('Says hello to someone"');        
+
+  client.application.commands.create(ping, "1209532674558140516");
+
+  client.application.commands.create(hello, "1209532674558140516");
+
+});
+
+client.on(Events.InteractionCreate, interaction => {
+  if(!interaction.isChatInputCommand()) return;
+  if(interaction.commandName === "ping"){
+    interaction.reply("Pong!");
+  }
+  if(interaction.commandName === "hello"){
+    interaction.reply(`Hello ${interaction.user.username}`)
+  }
+});
 
 client.login(token);

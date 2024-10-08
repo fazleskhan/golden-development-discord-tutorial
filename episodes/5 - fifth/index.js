e95b75ffe2f7b214d7b327ef5ed43f5a0cd8c0dc
Fazle Khan <fazleskhan@gmail.com>
2024-10-07 20:47:05 -0400

    added ready and interactionCreate events handler

--- episodes/5 - fifth/index.js
+++ episodes/5 - fifth/index.js
@@ -3,58 +3,25 @@ console.log("Hello World!");
 const { token } = require("./config.json");
 const { Client, Events, GatewayIntentBits, Collection } = require('discord.js');
 const fs = require("node:fs");
+const path = require('node:path');
 
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-client.commands = getCommands('./commands');
+const eventsPath = path.join(__dirname, 'events');
+eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-client.once(Events.ClientReady, c => {
-  console.log(`Logged in as ${c.user.tag}`);
-});
-
-client.on(Events.InteractionCreate, interaction => {
-  if(!interaction.isChatInputCommand()) return;
-
-  let command = client.commands.get(interaction.commandName);
-
-  try{
-    if(interaction.replied) return;
-    command.execute(interaction);    
-  }catch(error){
-    console.error(error);
+for(const file of eventFiles){
+  const filePath = path.join(eventsPath,file);
+  const event = require(filePath);
+  if(event.once){
+    client.once(event.name, (...args) => event.execute(...args));
+  }else{
+    client.on(event.name, (...args) => event.execute(...args));
   }
-});
+}
 
 client.login(token);
 
-function getCommands(dir){
-  let commands = new Collection();
-  const commandFiles = getFiles(dir);
-
-  for(const commandFile of commandFiles){
-    const command = require(commandFile);
-    commands.set(command.data.toJSON().name, command);
-  }
-  return commands;
-}
 
 
-function getFiles(dir) {
-  const files = fs.readdirSync(dir, {
-    withFileTypes: true
-  });
-  let commandFiles = [];
-
-  for(const file of files){
-    if(file.isDirectory()){
-      commandFiles = [
-        ...commandFiles,
-        ...getFiles(`${dir}/${file.name}`)
-      ]
-    }else if(file.name.endsWith(".js")){
-      commandFiles.push(`${dir}/${file.name}`)
-    }
-  }
-  return commandFiles;
-}
